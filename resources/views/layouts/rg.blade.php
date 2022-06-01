607da7dae769876c7bb00aaf1ebfb0a34a4f8aab
Miras <91594617+laqube@users.noreply.github.com>
2022-06-01 14:58:19 +0600

    Registration and Login
    Styles fixed

--- resources/views/layouts/rg.blade.php
+++ resources/views/layouts/rg.blade.php
@@ -14,28 +14,27 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
 </head>
-<body style="text-align: center;">
-<h2>Registration</h2>
-
+<body style="text-align: center; background-color: #EEEEEE">
     <div class="container mt-5 mb-5">
-        <div class="row d-flex align-items-center justify-content-center">
-            <div class="col-md-6">
-                <div class="card px-5 py-5" style="margin: 5rem; "> <span class="circle"><i class="fa fa-check"></i></span>
+        <h1 style="color: #393E46">Registration</h1>
+        <div class="row d-flex align-items-center justify-content-center" >
+            <div class="col-md-6" >
+                <div class="card px-5 py-5" style="margin: 5rem;"> <span class="circle"><i class="fa fa-check"></i></span>
                     <form action="save_user.php" method="post">
-                        <div class="form-input">
+                        <div class="form-input my-2">
                             <i class="fa fa-envelope"></i>
                             <input name="login" class="form-control" type="text" size="15" maxlength="15" placeholder="Email address" > </div>
-                        <div class="form-input">
+                        <div class="form-input my-2">
                             <i class="fa fa-user"></i>
                             <input name="nickname" class="form-control" type="text" size="16" maxlength="16" placeholder="User name"> </div>
-                        <div class="form-input">
+                        <div class="form-input my-2">
                             <i class="fa fa-lock"></i>
                             <input name="password" type="password" class="form-control"  size="15" maxlength="15" placeholder="Password"> </div>
                         <button class="btn btn-secondary mt-4">
-                            <a href="/Shop" style="color:#EEE"><input type="submit" name="submit" value="Sign up"></a></button>
-                        <div class="text-center mt-3"> <span>Or continue with these social profile</span> </div>
+                            <input type="submit" name="submit" value="Sign up">
+                        </button>
                         <div class="d-flex justify-content-center mt-4"> <span class="social"><i class="fa fa-google"></i></span> <span class="social"><i class="fa fa-facebook"></i></span> <span class="social"><i class="fa fa-twitter"></i></span> <span class="social"><i class="fa fa-linkedin"></i></span> </div>
-                        <div class="text-center mt-4"> <span>Already a member?</span> <a href="/sn" class="text-decoration-none" style="color:#EEEEEE">Login</a> </div>
+                        <div class="text-center mt-4"> <span>Already a member?</span> <a href="/sn" class="text-decoration-none" style="color:black">Login</a> </div>
                     </form>
                 </div>
             </div>
